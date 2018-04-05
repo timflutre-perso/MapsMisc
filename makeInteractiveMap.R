c5d1d63982a88e7241becda6ddd5fda800aa1ded
Timothée Flutre <timflutre@gmail.com>
2018-04-05 22:54:30 +0200

    allow to decrease photo resolution + minor improvements

--- makeInteractiveMap.R
+++ makeInteractiveMap.R
@@ -1,4 +1,4 @@
-## Copyright (C) 2017 Timothée Flutre
+## Copyright (C) 2017-2018 Timothée Flutre
 ## License AGPL-3 https://www.gnu.org/licenses/agpl.html
 
 ##' Make an interactive map
@@ -9,6 +9,7 @@
 ##' @param zoom zoom of the map
 ##' @param provinces.file optional path to the file containing the provinces in GeoJSON format
 ##' @param photos.glob optional glob to find all photos to add to the map, for instance \code{"photos/*.JPG"}
+##' @param img.max.height maximum height in pixels for the output images (if NULL, no proportional resizing will be performed)
 ##' @param cadastre.file optional path to the file containing the cadastre in GeoJSON format
 ##' @param out.dir path to the output directory (if NULL, will be the current directory)
 ##' @param thumbnails.width width of the thumbnail photos in the popups
@@ -38,32 +39,45 @@ makeInteractiveMap <- function(center.lng=108.194733,
                                zoom=5,
                                provinces.file=NULL,
                                photos.glob=NULL,
+                               img.max.height=1200,
                                cadastre.file=NULL,
                                out.dir=NULL,
-                               thumbnails.width=300, thumbnails.height=300,
+                               thumbnails.width=400, thumbnails.height=300,
                                verbose=1){
+  if(is.null(out.dir))
+    out.dir <- paste0(getwd(), "/interactive-map")
+  if(dir.exists(out.dir)){
+    msg <- paste0("output directory '", out.dir, "' already exists")
+    stop(msg)
+  }
   requireNamespace("leaflet")
   requireNamespace("htmlwidgets")
-  if(! is.null(provinces.file) || ! is.null(cadastre.file))
+  if(! is.null(photos.glob) & ! is.null(img.max.height))
+    requireNamespace("magick")
+  if(! is.null(provinces.file)){
+    if(! file.exists(provinces.file)){
+      msg <- paste0("provinces.file '", provinces.file, "' doesn't exist")
+      warning(msg, immediate.=TRUE)
+      provinces.file <- NULL
+    }
+  }
+  if(! is.null(cadastre.file)){
+    if(! file.exists(cadastre.file)){
+      msg <- paste0("cadastre.file '", cadastre.file, "' doesn't exist")
+      warning(msg, immediate.=TRUE)
+      cadastre.file <- NULL
+    }
+  }
+  if(! is.null(provinces.file) | ! is.null(cadastre.file))
     requireNamespace("geojsonio")
   if(! is.null(photos.glob)){
     requireNamespace("exifr")
     requireNamespace("mapview")
   }
-  if(! is.null(provinces.file))
-    stopifnot(file.exists(provinces.file))
-  if(! is.null(cadastre.file))
-    stopifnot(file.exists(cadastre.file))
 
-  if(is.null(out.dir))
-    out.dir <- paste0(getwd(), "/interactive-map")
   if(verbose > 0){
     msg <- paste0("create the output directory '", out.dir, "'...")
     write(msg, stdout())
-  }
-  if(dir.exists(out.dir)){
-    msg <- paste0("output directory '", out.dir, "' already exists")
-    stop(msg)
   }
   dir.create(out.dir)
 
@@ -89,11 +103,24 @@ makeInteractiveMap <- function(center.lng=108.194733,
       msg <- "no photos were found, check 'photos.glob'"
       warning(msg)
     } else{
-      file.copy(photos, photos.dir)
-      ## use pkg jpeg or imager to copy files in smaller resolution?
+      if(is.null(img.max.height)){
+        file.copy(photos, photos.dir)
+      } else{
+        isImgToTall <- function(img, img.max.height){
+          magick::image_info(img)$height > img.max.height
+        }
+        for(i in seq_along(photos)){
+          img <- magick::image_read(photos[i])
+          if(isImgToTall(img, img.max.height))
+            img <- magick::image_scale(img, paste0("x", img.max.height))
+          p2f.new <- paste0(photos.dir, "/", basename(photos[i]))
+          stopifnot(! file.exists(p2f.new))
+          magick::image_write(img, path=p2f.new, format="jpeg")
+        }
+      }
       tmp <- list.files(photos.dir, full.names=TRUE)
-      dat <- exifr::exifr(tmp, recursive=TRUE,
-                          quiet=ifelse(verbose <= 0, TRUE, FALSE))
+      dat <- exifr::read_exif(tmp, recursive=FALSE,
+                              quiet=ifelse(verbose <= 0, TRUE, FALSE))
       stopifnot(all(c("SourceFile", "DateTimeOriginal", "GPSLongitude",
                       "GPSLatitude") %in% colnames(dat)))
       dat <- dat[, c("SourceFile", "DateTimeOriginal",
